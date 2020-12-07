5b2581f1bc8021a45400ce157fdf92376b851609
Ion Arțăriși <ionut@artarisi.eu>
2020-12-07 18:16:11 +0000

    2020/7 non-working refactor

--- 7.py
+++ 7.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from collections import defaultdict
 import re
 
@@ -9,7 +10,6 @@ muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
 shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
 dark olive bags contain 3 faded blue bags, 4 dotted black bags.
 vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
-vibrant flap bags contain 1 muted yellow bags.
 faded blue bags contain no other bags.
 dotted black bags contain no other bags."""
 
@@ -18,32 +18,23 @@ def load_rules(rules):
     for rule in rules:
         if not rule or rule =='\n':
             continue
-        try:
-            groups = re.match("(\w+ \w+) bags contain (?:\d (\w+ \w+) bag[s]*(?:, \d (\w+ \w+) bag[s]*)?|no other bags).", rule).groups()
-        except:
-            import pdb;pdb.set_trace()
-        for group in groups[1:]:
-            if group:
-                rs[group].append(groups[0])
+
+        groups = re.match("(\w+ \w+) bags contain (?:\d (\w+ \w+) bag[s]*(?:, \d (\w+ \w+) bag[s]*)?|no other bags).", rule).groups()
+
+        rs[groups[0]] = groups[1:]
 
     return rs
 
 
+def has_shiny_gold(rules, bag):
+    if bag == 'shiny gold':
+        return True
 
-def find_bags(rules, bag):
-    try:
-        next_bags = rules[bag]
-    except KeyError:
-        return []
-
-    ret = set(next_bags + [b for bag in next_bags for b in find_bags(rules, bag)])
-    print(bag, ret)
-    return ret
+    return any(has_shiny_gold(rules, b) for b in rules[bag])
 
 rules = load_rules(rules_file.splitlines())
-print(find_bags(rules, 'shiny gold'))
-len(find_bags(rules, 'shiny gold'))
-# with open('7.input') as f:
-#     rules = load_rules(f.readlines())
-#     print(find_bags(rules, 'shiny gold'))
-#     print(len(find_bags(rules, 'shiny gold')))
+sum(has_shiny_gold(deepcopy(rules), b) for b in rules) - 1
+with open('7.input') as f:
+    rules = load_rules(f.readlines())
+    print(sum(has_shiny_gold(deepcopy(rules), b) for b in rules) - 1)
+
