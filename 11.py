5823db4a132a917a7bf773ccdfc788b2dcff2113
Ionuț Arțăriși <ionut@artarisi.eu>
2020-12-11 19:37:29 +0000

    ugly 2020/11 gold

--- 11.py
+++ 11.py
@@ -9,6 +9,16 @@ LLLLLLLLLL
 L.LLLLLL.L
 L.LLLLL.LL"""
 
+example1 = """.......#.
+...#.....
+.#.......
+.........
+..#L....#
+....#....
+.........
+#........
+...#....."""
+
 
 def read_input(lines):
     positions = []
@@ -21,7 +31,7 @@ def read_input(lines):
     return positions
 
 
-def one_step(positions):
+def silver_step(positions):
     next_step = []
     for i, line in enumerate(positions):
         next_line = []
@@ -50,6 +60,53 @@ def one_step(positions):
     return next_step
 
 
+def gold_step(positions):
+    next_step = []
+    for i, line in enumerate(positions):
+        next_line = []
+        next_step.append(next_line)
+        for j, p in enumerate(line):
+            # if i != 4 or j != 3:
+            #     continue
+
+            if p == '.':
+                next_line.append('.')
+                continue
+
+            seen_occupied = 0
+            # print("i:", i, "j:", j, end=" ")
+            for x, y in [
+                    (0, 1),
+                    (1, 0),
+                    (1, 1),
+                    (-1, -1),
+                    (-1, 0),
+                    (0, -1),
+                    (-1, 1),
+                    (1, -1)
+            ]:
+                ii = i + x
+                jj = j + y
+                while 0 <= ii < len(positions) and 0 <= jj < len(line):
+                    if positions[ii][jj] == '#':
+                        # print(x, y, ii, jj)
+                        seen_occupied += 1
+                        break
+                    if positions[ii][jj] == 'L':
+                        break
+                    ii += x
+                    jj += y
+
+            if p == 'L' and seen_occupied == 0:
+                next_line.append('#')
+            elif p == '#' and seen_occupied >= 5:
+                next_line.append('L')
+            else:
+                next_line.append(p)
+
+    return next_step
+
+
 def show(positions):
     for line in positions:
         for p in line:
@@ -58,13 +115,12 @@ def show(positions):
     print()
 
 
-def stabilize(positions):
+def stabilize(positions, step_func):
     prev = 0
     curr = positions
     while prev != curr:
         prev = curr
-        curr = one_step(curr)
-
+        curr = step_func(curr)
 
     return curr
 
@@ -74,9 +130,12 @@ def count_occupied(positions):
 
 
 pos = read_input(example.splitlines())
-count_occupied(stabilize(pos))
+print("example:", count_occupied(stabilize(pos, gold_step)))
 
 with open('11.input') as f:
-    silver = count_occupied(stabilize(read_input(f.readlines())))
+    silver = count_occupied(stabilize(read_input(f.readlines()), silver_step))
+with open('11.input') as f:
+    gold = count_occupied(stabilize(read_input(f.readlines()), gold_step))
 
-print("silver: ", silver )
+print("silver: ", silver)
+print("gold: ", gold)
